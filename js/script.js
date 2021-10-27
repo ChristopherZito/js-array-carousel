e1014738149d8b818e376a76baea2ae78b17a5da
ChristopherZito <87862547+ChristopherZito@users.noreply.github.com>
2021-10-27 20:09:08 +0200

    end

--- js/script.js
+++ js/script.js
@@ -32,15 +32,17 @@ let left = "";
 // thumbs
 let right = "";
 
+// output in HTML
 for(let i = 0; i < items.length; i++){
-    left += `    
+    left += `   
+    
         <div class="object">
             <img src="${items[i]}" alt="${title[i]}">
-        </div>
-        <div class="text">
-            <h2 >${title[i]} </h2>
-            <p>${text[i]}</p>
-        </div>` ;  
+            <div class="text">
+                <h2 >${title[i]} </h2>
+                <p>${text[i]}</p>
+            </div>  
+        </div>`;
     
     right += `        
         <div class="sub-img">
@@ -48,6 +50,55 @@ for(let i = 0; i < items.length; i++){
         </div> `;
 }
 
+// output in page 
 immage.innerHTML += left;
+document.getElementsByClassName("object")[0].classList.add("active");
 imgPreview.innerHTML += right;
+document.getElementsByClassName("sub-img")[0].classList.add("active");
+
+// bottoni 
+let pos = 0;
+
+
+// next
+const next = document.querySelector(".next");
+
+next.addEventListener("click", 
+function(){
+    pos = pos + 1;
+    if(pos > 4){
+        pos = 0
+    };
+    console.log(pos);
+    document.querySelector(".sub-img.active").classList.remove("active");
+    document.getElementsByClassName("sub-img")[pos].classList.add("active");
+
+    document.querySelector(".object.active").classList.remove("active");
+    document.getElementsByClassName("object")[pos].classList.add("active");
+
+    
+    
+}
+);
+
+// before
+const before = document.querySelector(".before");
+
+before.addEventListener("click", 
+function(){
+    pos = pos - 1;
+    if(pos < 0){
+        pos = 4
+    };
+    console.log(pos);
+    document.querySelector(".sub-img.active").classList.remove("active");
+    document.getElementsByClassName("sub-img")[pos].classList.add("active");
+
+    document.querySelector(".object.active").classList.remove("active");
+    document.getElementsByClassName("object")[pos].classList.add("active");  
+
+    
+}
+);
+
 
